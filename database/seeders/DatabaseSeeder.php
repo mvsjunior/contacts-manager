426d7061f9a5f583b61c872ab5fab57f44d37ac3
mauro <mauro.emc@outlook.com>
2025-11-24 14:23:10 -0300

    Seeder for users table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,13 +11,8 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    public function run(): void
+    public function run(UserSeeder $userSeeder): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $userSeeder->run();
     }
 }
